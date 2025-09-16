e348237e5ac187b17e8c54d8ada9b11b343abeed
Ivan Ramos <ivanramos@menorcaon.com>
2025-09-16 14:39:13 +0200

    [feat] hook action

--- htdocs/custom/verifactu/class/actions_verifactu.class.php
+++ htdocs/custom/verifactu/class/actions_verifactu.class.php
@@ -9,25 +9,30 @@ class ActionsVerifactu
     public function formObjectOptions($parameters, &$object, &$action, $hookmanager)
     {
         global $langs, $db;
+        var_dump($parameters);
+        die();
 
         if ($parameters['currentcontext'] === 'invoicecard') {
             $langs->load("verifactu@verifactu");
 
-            // Cargar lista de tipos
-            $sql = "SELECT rowid, code, label FROM ".MAIN_DB_PREFIX."verifactu_facturae_type WHERE active = 1";
+            $sql = "SELECT rowid, code, label 
+                    FROM ".MAIN_DB_PREFIX."verifactu_facture_type 
+                    WHERE active = 1
+                    ORDER BY code";
             $resql = $db->query($sql);
 
             $options = '';
             if ($resql) {
                 while ($obj = $db->fetch_object($resql)) {
-                    $selected = ($object->fk_facturae_type == $obj->rowid ? 'selected' : '');
+                    $selected = ($object->fk_facture_type == $obj->rowid ? 'selected' : '');
                     $options .= '<option value="'.$obj->rowid.'" '.$selected.'>['.$obj->code.'] '.$obj->label.'</option>';
                 }
             }
 
-            print '<tr><td><span class="fieldrequired">'.$langs->trans("FacturaeType").'</span></td>';
-            print '<td><select name="fk_facturae_type" required>';
-            print '<option value="">-- '.$langs->trans("SelectFacturaeType").' --</option>';
+            print '<tr class="oddeven">';
+            print '<td><span class="fieldrequired">'.$langs->trans("FactureType").'</span></td>';
+            print '<td><select name="fk_facture_type" required>';
+            print '<option value="">-- '.$langs->trans("SelectFactureType").' --</option>';
             print $options;
             print '</select></td></tr>';
         }
