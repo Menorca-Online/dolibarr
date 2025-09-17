43b92d7f81371bd1d672eab9ef5eff515f5d1a2a
Joel Martínez <joel.mh112@gmail.com>
2025-09-17 08:18:50 +0200

    [DELETE] deleted .sql files

--- htdocs/custom/verifactu/sql/llx_facture_add_fk_facture_type.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE llx_facture
-ADD COLUMN fk_facture_type INTEGER NULL;
-
-ALTER TABLE llx_facture
-ADD CONSTRAINT fk_facture_facture_type FOREIGN KEY (fk_facture_type)
-REFERENCES llx_verifactu_facture_type (rowid);
