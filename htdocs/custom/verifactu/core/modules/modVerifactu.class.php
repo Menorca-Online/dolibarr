1b86a70feeb02d25f9074b8dfa2f3f5309c16a7f
Ivan Ramos <ivanramos@menorcaon.com>
2025-09-25 13:28:08 +0200

    [feat]

--- htdocs/custom/verifactu/core/modules/modVerifactu.class.php
+++ htdocs/custom/verifactu/core/modules/modVerifactu.class.php
@@ -936,6 +936,13 @@ class modVerifactu extends DolibarrModules
 			return -1;
 		}
 
+		$sql = "DROP TABLE IF EXISTS " . MAIN_DB_PREFIX . "verifactu_factura_registros";
+		$resql = $this->db->query($sql);
+		if (! $resql) {
+			dol_print_error($this->db);
+			return -1;
+		}
+
 		return 1;
 	}
 
