e354259aff76887e9ad18fc830d348b79d81c342
Joel Martínez <joel.mh112@gmail.com>
2025-10-07 13:52:20 +0200

    [FIX]

--- htdocs/custom/verifactu/class/verifactuxml.class.php
+++ htdocs/custom/verifactu/class/verifactuxml.class.php
@@ -883,7 +883,7 @@ class VerifactuXML
      */
     private function processResponse($response)
     {
-        global $user;
+        global $user, $conf;
         if (empty($response)) {
             return;
         }
@@ -961,8 +961,8 @@ class VerifactuXML
             // Extraer TiempoEsperaEnvio
             $tiempoEsperaEnvioNode = $xpath->query('//tikR:TiempoEsperaEnvio')->item(0);
             if ($tiempoEsperaEnvioNode) {
-                //tiempo en segundos, sumarselo a la hora actual para saber cuando volver a enviar
-                $this->batch->tiempo_espera_envio = (int) $tiempoEsperaEnvioNode->textContent + time();
+                //tiempo en segundos, sumarselo a la hora actual para saber cuando volver a enviar y guardar ese valor en la config global
+                $conf->global->VERIFACTU_PROXIMO_ENVIO = (int) $tiempoEsperaEnvioNode->textContent + time();
             }
 
             // Extraer EstadoEnvio
